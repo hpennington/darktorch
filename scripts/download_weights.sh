928b55175354dec8a8f0ac3aacf37f3be07bb567
Hayden Pennington <haydenpennington@icloud.com>
2019-09-21 15:21:41 -0500

    Update weights script

--- scripts/download_weights.sh
+++ scripts/download_weights.sh
@@ -3,5 +3,6 @@
 wget https://pjreddie.com/media/files/yolov2.weights
 wget https://pjreddie.com/media/files/yolov2-voc.weights
 wget https://pjreddie.com/media/files/yolov2-tiny.weights
+wget https://pjreddie.com/media/files/yolov2-tiny-voc.weights
 wget https://pjreddie.com/media/files/yolov3.weights
 wget https://pjreddie.com/media/files/yolov3-tiny.weights
