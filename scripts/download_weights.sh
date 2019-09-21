05f81f0a08c5b98e2bef939795ef339725490eb9
Hayden Pennington <haydenpennington@icloud.com>
2019-09-21 14:51:47 -0500

    Add Download weights script

---
+++ scripts/download_weights.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://pjreddie.com/media/files/yolov2.weights
+wget https://pjreddie.com/media/files/yolov2-voc.weights
+wget https://pjreddie.com/media/files/yolov2-tiny.weights
+wget https://pjreddie.com/media/files/yolov3.weights
+wget https://pjreddie.com/media/files/yolov3-tiny.weights
